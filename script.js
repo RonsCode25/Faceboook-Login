8ce4810f444ef0127f1ed33ff703e4975a9301da
RonsCode25 <ronscode25@gmail.com>
2025-02-14 14:35:02 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+document.querySelector('.file').addEventListener('change', function () {
+    const file = this.files[0];
+
+    if (!file.type.match('image/jpeg')) {
+        alert('Please upload a JPEG image.');
+        this.value = '';
+        return;
+    }
+
+    const reader = new FileReader();
+    reader.onload = function (event) {
+        document.querySelector('.pic').style.backgroundImage = `url('${event.target.result}')`;
+    };
+    reader.readAsDataURL(file);
+});
+
+function displayPromp(){
+    document.querySelector('.form').style.display="none";
+    document.querySelector('.prank').style.display="flex";
+    const input=document.querySelector('#trgtName');
+    const output=document.querySelector('#txtContent');
+    const name=input.value.trim()||'NO NAME PROVIDED';
+
+    output.textContent=`
+        We'll save the login info for ${name} to your
+        device's cloud backup, so you wont need to enter it on 
+        this device or any device you restore.
+    `;
+}
+function displayForm(){
+    document.querySelector('.form').style.display="flex";
+    document.querySelector('.prank').style.display="none";
+}
